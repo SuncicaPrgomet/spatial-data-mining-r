900dcf723804103a0952dd86905e7d4b945d8d28
Sunčica Prgomet <suncica.prgomet1@gmail.com>
2026-09-05 17:42:08 +0200

    Create README.m

---
+++ exercises/README.m
@@ -0,0 +1,5 @@
+# Supporting R Exercises
+
+Foundational R and data mining exercises completed as part of the course.
+
+These scripts cover basic R programming, exploratory data analysis, matrix operations, clustering, and decision tree regression, and serve as supporting exercises for the larger spatial analyses in this repository.
